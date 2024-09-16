77bb2853769edc31a17be3290365db18a3cae05c
Nortey-Micheal <norteymicheal91@gmail.com>
2024-09-16 11:52:38 -0700

    "Added CSS styles, HTML structure, and JavaScript code for a personal website, including a header, main section, and footer, with animations and image sources."

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './index.css';
+import profile from './profile.jpg'
+import blog from './images/blog-preview-card.png';
+import recipe from './images/recipe-page3.png';
+import rock from './images/ROCK_PAPER_SCISSORS.png';
+import social from './images/social-links-profile.png';
 
-console.log("New Project");
+const img = document.querySelector('.skewed>div>div>img');
+img.src = profile
+
+const blog1 = document.querySelector('.works img:first-of-type');
+blog1.src = blog
+const recipe1 = document.querySelector('.works>div:nth-of-type(2)>img');
+recipe1.src = recipe
+const rock1 = document.querySelector('.works>div:nth-of-type(3)>img');
+rock1.src = rock
+const social1 = document.querySelector('.works>div:nth-of-type(4)>img');
+social1.src = social
