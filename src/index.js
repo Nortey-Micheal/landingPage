e0095159c6965cff35954cbe34af2eba4ac77cf5
Nortey-Micheal <norteymicheal91@gmail.com>
2024-09-16 13:56:11 -0700

    feat: modified styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import blog from './images/blog-preview-card.png';
 import recipe from './images/recipe-page3.png';
 import rock from './images/ROCK_PAPER_SCISSORS.png';
 import social from './images/social-links-profile.png';
+import office from './images/office.jpg'
 
 const img = document.querySelector('.skewed>div>div>img');
 img.src = profile
@@ -16,3 +17,6 @@ const rock1 = document.querySelector('.works>div:nth-of-type(3)>img');
 rock1.src = rock
 const social1 = document.querySelector('.works>div:nth-of-type(4)>img');
 social1.src = social
+
+const office1 = document.querySelector('footer img')
+office1.src = office
